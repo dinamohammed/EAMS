5b774131caa1ee4a8fec1594914719a4497f7635
tagelmaaly <tagelmaaly@gmail.com>
2021-03-11 11:35:57 +0200

    Adding Inventory Adjustement Excel report Arabic Adjusting Inventory Adjustement Excel report (Note: there is a BUG whene printing 1 record raise error + _ liberary is not working to translate the xlsx report need to fix it)

--- egymentors_inventory/reports/__init__.py
+++ egymentors_inventory/reports/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import purchase_requistion_report
 from . import inventory_adjustment_xlsx
+from . import inventory_adjustment_arabic_xlsx
--- egymentors_inventory/reports/inventory_adjustment_arabic_xlsx.py
+++ egymentors_inventory/reports/inventory_adjustment_arabic_xlsx.py
@@ -1,21 +1,20 @@
-from odoo import models, _
+from odoo import models
 
 
-class InventoryAdjustmentXLSX(models.AbstractModel):
-    _name = 'report.egymentors_inventory.report_inventory_adjustment_xlsx'
+class InventoryAdjustmentArabicXLSX(models.AbstractModel):
+    _name = 'report.egymentors_inventory.report_inventory_adjustment_ar_xlsx'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, adjustment):
-        # self.with_context(lang=self.env.user.lang)
-        worksheet = workbook.add_worksheet(_('Inventory Adjustment'))
+        self.with_context(lang=self.env.user.lang)
+        worksheet = workbook.add_worksheet('Inventory Adjustment')
 
         # Format
         worksheet.set_column('A:A', 18)
         worksheet.set_column('B:B', 12)
         worksheet.set_column('C:C', 22)
-        worksheet.set_column('E:E', 10)
-        worksheet.set_column('F:F', 11)
-        worksheet.set_column('H:H', 11)
+        worksheet.set_column('E:E', 12)
+        worksheet.set_column('H:H', 10)
         worksheet.set_column('J:J', 12)
         sheet_header = workbook.add_format(
             {'font_size': 14, 'bold': True, 'align': 'center', 'valign': 'vcenter', 'fg_color': '#898a8c'})
@@ -25,63 +24,65 @@ class InventoryAdjustmentXLSX(models.AbstractModel):
         format_value_bold_left = workbook.add_format(
             {'font_size': 10, 'align': 'left', 'bold': True, 'fg_color': '#E4E2E2'})
         format_value = workbook.add_format({'font_size': 10, 'align': 'center', 'fg_color': '#FFFFFF'})
-        format_header_label = workbook.add_format(
+        format_header_label_right = workbook.add_format(
             {'font_size': 10, 'align': 'right', 'bold': True, 'fg_color': '#898a8c'})
-        format_header_value = workbook.add_format({'font_size': 10, 'align': 'left', 'fg_color': '#898a8c'})
+        format_header_label_left = workbook.add_format(
+            {'font_size': 10, 'align': 'left', 'bold': True, 'fg_color': '#898a8c'})
+        format_header_value_right = workbook.add_format({'font_size': 10, 'align': 'right', 'fg_color': '#898a8c'})
 
         # Sheet Header
         row = 0
         column = 0
-        worksheet.write(row, column, _('Authority Name: '), format_header_label)
-        worksheet.write(row, column + 1, '', format_header_label)
-        worksheet.write(row, column + 2, '', format_header_label)
-        worksheet.write(row, column + 3, '', format_header_label)
+        worksheet.write(row, column, 'اسم الجهة', format_header_label_right)
+        worksheet.write(row, column + 1, '', format_header_label_right)
+        worksheet.write(row, column + 2, '', format_header_label_right)
+        worksheet.write(row, column + 3, '', format_header_label_right)
         row += 1
-        worksheet.write(row, column, _('Warehouse: '), format_header_label)
+        worksheet.write(row, column, 'المخزن', format_header_label_right)
         location_list = adjustment.location_ids.mapped('name')
-        worksheet.merge_range(row, column + 1, row, column + 3, ', '.join(location_list), format_header_value)
+        worksheet.merge_range(row, column + 1, row, column + 3, ', '.join(location_list), format_header_value_right)
         row += 1
-        worksheet.write(row, column, _('Inventory Date From: '), format_header_label)
+        worksheet.write(row, column, 'تاريخ الجرد من', format_header_label_right)
         date_list = [d.strftime('%d-%m-%Y') for d in adjustment.mapped('date')]
-        worksheet.write(row, column + 1, max(date_list), format_header_value)
-        worksheet.write(row, column + 2, _('Date To: '), format_header_label)
-        worksheet.write(row, column + 3, min(date_list), format_header_value)
+        worksheet.write(row, column + 1, max(date_list), format_header_value_right)
+        worksheet.write(row, column + 2, 'الي', format_header_label_right)
+        worksheet.write(row, column + 3, min(date_list), format_header_value_right)
         row = 0
-        worksheet.merge_range(row, column + 4, row + 2, column + 7, _("Inventory Adjustment"), sheet_header)
-        worksheet.merge_range(row, column + 8, row + 2, column + 8, '', format_header_label)
-        worksheet.merge_range(row, column + 9, row, column + 11, _('(Form No. 9 Inventory government)'),
-                              format_header_label)
+        worksheet.merge_range(row, column + 4, row + 2, column + 7, "محضر جرد الاصناف", sheet_header)
+        worksheet.merge_range(row, column + 8, row + 2, column + 8, '', format_header_label_right)
+        worksheet.merge_range(row, column + 9, row, column + 11, '(نموزج 9 مخازن حكومة)',
+                              format_header_label_left)
         row += 1
-        worksheet.merge_range(row, column + 9, row, column + 11, '', format_header_label)
+        worksheet.merge_range(row, column + 9, row, column + 11, '', format_header_label_right)
         row += 1
-        worksheet.merge_range(row, column + 9, row, column + 11, _("Custody Keeper Name: .............."),
-                              format_header_label)
+        worksheet.merge_range(row, column + 9, row, column + 11, "اسم صاحب العهدة",
+                              format_header_label_right)
 
         # Table Header
         row += 2
-        worksheet.write(row, column, _('Date'), format_label)
+        worksheet.write(row, column, 'التاريخ', format_label)
         column += 1
-        worksheet.write(row, column, _('Code'), format_label)
+        worksheet.write(row, column, 'رقم الصنف', format_label)
         column += 1
-        worksheet.write(row, column, _('Product Name'), format_label)
+        worksheet.write(row, column, 'اسم الصنف', format_label)
         column += 1
-        worksheet.write(row, column, _('UoM'), format_label)
+        worksheet.write(row, column, 'الوحدة', format_label)
         column += 1
-        worksheet.write(row, column, _('Counted'), format_label)
+        worksheet.write(row, column, 'الموجود من واقع الجرد', format_label)
         column += 1
-        worksheet.write(row, column, _('Product State'), format_label)
+        worksheet.write(row, column, 'حالة الصنف', format_label)
         column += 1
-        worksheet.write(row, column, _('On Hand'), format_label)
+        worksheet.write(row, column, 'الرصيد الدفتري', format_label)
         column += 1
-        worksheet.write(row, column, _('Product State'), format_label)
+        worksheet.write(row, column, 'حالة الصنف', format_label)
         column += 1
-        worksheet.write(row, column, _('Cost'), format_label)
+        worksheet.write(row, column, 'التكلفة', format_label)
         column += 1
-        worksheet.write(row, column, _('Difference'), format_label)
+        worksheet.write(row, column, 'الفارق', format_label)
         column += 1
-        worksheet.write(row, column, _('Value'), format_label)
+        worksheet.write(row, column, 'القيمة', format_label)
         column += 1
-        worksheet.write(row, column, _('State'), format_label)
+        worksheet.write(row, column, 'الحالة', format_label)
 
         # Adjustment Lines
         for line in adjustment:
