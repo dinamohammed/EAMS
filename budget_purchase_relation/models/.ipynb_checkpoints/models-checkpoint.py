675d1856b088d6fc2516453fd67c35bee35e5b62
dinamohammed <dina.mohammed392@gmail.com>
2021-04-05 16:20:28 +0000

    multi

--- budget_purchase_relation/models/.ipynb_checkpoints/models-checkpoint.py
+++ budget_purchase_relation/models/.ipynb_checkpoints/models-checkpoint.py
@@ -50,8 +50,8 @@ class PurchaseOrderLine(models.Model):
     
     reserve_budget_entry = fields.Many2one(related='order_id.reserve_budget_entry',store=True)
     
-    def _prepare_account_move_line(self, move):
-        res = super(PurchaseOrderLineInherit, self)._prepare_account_move_line(move)
+    def _prepare_account_move_line(self):
+        res = super(PurchaseOrderLine, self)._prepare_account_move_line()
         self.ensure_one()
         
         res['budget_id'] = self.order_id.budget_id.id
@@ -172,6 +172,18 @@ class AccountMove(models.Model):
     reserve_budget_entry = fields.Many2one(related='invoice_line_ids.reserve_budget_entry',store=True)
     commitment_budget_entry = fields.Many2one(related='invoice_line_ids.reserve_budget_entry',store=True)
     
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        # OVERRIDE
+        moves = super(AccountMove, self).create(vals_list)
+        for move in moves:
+            for line in move.line_ids:
+                if line.purchase_line_id:
+                    line['budget_id'] = line.purchase_line_id.budget_id.id
+                    line['reserve_budget_entry'] = line.purchase_line_id.reserve_budget_entry.id
+        return moves
+    
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -182,39 +194,28 @@ class AccountMoveLine(models.Model):
     commitment_budget_entry = fields.Many2one('budget.entry','Commitment Budget Entry')
 
     
-    @api.model_create_multi
-    def create(self, vals_list):
-        # OVERRIDE
-        moves = super(AccountInvoiceInherit, self).create(vals_list)
-        for move in moves:
-            for line in move.line_ids:
-                if line.purchase_line_id:
-                    line['budget_id'] = line.purchase_line_id.budget_id.id
-                    line['reserve_budget_entry'] = line.purchase_line_id.reserve_budget_entry.id
-        return moves
-    
     
 class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
     
-    budget_id = fields.Many2one('crossovered.budget', 'Budget',  readonly=True, copy=False)
+#     budget_id = fields.Many2one('crossovered.budget', 'Budget',  readonly=True, copy=False)
     
     ####### Override method to add budget_id
-    def _create_payment_vals_from_wizard(self):
-        payment_vals = {
-            'date': self.payment_date,
-            'amount': self.amount,
-            'payment_type': self.payment_type,
-            'partner_type': self.partner_type,
-            'ref': self.communication,
-            'journal_id': self.journal_id.id,
-            'currency_id': self.currency_id.id,
-            'partner_id': self.partner_id.id,
-            'partner_bank_id': self.partner_bank_id.id,
-            'payment_method_id': self.payment_method_id.id,
-            'destination_account_id': self.line_ids[0].account_id.id,
-            'budget_id': self.line_ids[0].budget_id.id
-        }
+#     def _create_payment_vals_from_wizard(self):
+#         payment_vals = {
+#             'date': self.payment_date,
+#             'amount': self.amount,
+#             'payment_type': self.payment_type,
+#             'partner_type': self.partner_type,
+#             'ref': self.communication,
+#             'journal_id': self.journal_id.id,
+#             'currency_id': self.currency_id.id,
+#             'partner_id': self.partner_id.id,
+#             'partner_bank_id': self.partner_bank_id.id,
+#             'payment_method_id': self.payment_method_id.id,
+#             'destination_account_id': self.line_ids[0].account_id.id,
+#             'budget_id': self.line_ids[0].budget_id.id
+#         }
         
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
