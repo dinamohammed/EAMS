bd76a9f12d509bbcaeca39bac370f9a6c7d38539
tagelmaaly <tagelmaaly@gmail.com>
2021-04-16 20:00:31 +0200

    TENDER:>>> Adding new sequence for tender. Renaming file purchase_request_date.xml to be sequence.xml. Adding Arabic translation to all parts of tender and some of purchase. Changing fields in Financial Signature to be text. Adding Technical Signature text. Renaming product_changes.py to be product_inherit.py. Adding Tender features in purchase order and purchase order line. Adding Tender features in purchase requisition and purchase requisition line. Adding and Adjusting Approval Cycle of the purchase request: buttons and request status. Adding and Adjusting Approval Cycle of the tender request: buttons and request status. Adding users responsible feature to purchase request: Manager can assign the employees who can view this purchase request. Adding users responsible feature to tender request: Manager can assign the employees who can view this tender request. Adding Record rule to purchase request: user can see his own requests or that assigned to him by the manager. Adding Record rule to tender request: user can see his own requests or that assigned to him by the manager. Adding Record rule to purchase requisition: user can see his own requests. Adding groups of users with defferent authorities: User - Auditor - Manager - General manager. Renaming purchase_order_view_changes.xml to be purchase_requisition_inherit.py. Renaming res_config_changes.py file to be res_config_inherit.py. Renaming stock_inventory_changes.py file to be stock_inventory_inherit.py. Renaming stock_picking_changes.py file to be stock_picking_inherit.py. Adding Tender Request, Tender Request Department and Tender Request Line models. Adding new report purchase_requisition_report_technical.py. Renaming purchase_requistion_report.py file to be purchase_requisition_report_financial.py. Change directory from views to reports of the following files: report_purchase_order_check.xml, report_purchase_order_inherit.xml, report_purchase_order_note.xml, report_purchase_request.xml, report_purchase_requisition_price.xml. Fixing som translation in the whole module. Renaming purchase_request_security.xml file to be security.xml. Renaming product_changes_view.xml file to be product_view_inherit.xml. Renaming res_config_view_changes.xml file to be res_config_view_inherit.xml. Renaming stock_inventory_view_changes.xml file to be stock_inventory_view_inherit.xml. Adding 'hr' module to the depends in the manifest file.

--- egymentors_inventory/models/agreement_signature.py
+++ egymentors_inventory/models/agreement_signature.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
-from odoo.exceptions import Warning
+from odoo import fields, models
 
 
 class AgreementSignature(models.Model):
-	_name = 'agreement.signature'
-	
-	name = fields.Many2one('hr.employee', 'Name')
-	title = fields.Char(related = 'name.job_id.name', store=True)
-	rank = fields.Integer('Rank',help="Add the Order of print out")
+    _name = 'agreement.signature'
+
+    name = fields.Char(string="Name")
+    title = fields.Char(string="Title")
+    rank = fields.Integer('Rank', help="Add the Order of print out")
+
+
+class AgreementSignatureTechnical(models.Model):
+    _name = 'agreement.signature.technical'
+
+    name = fields.Char(string="Name")
+    title = fields.Char(string="Title")
+    rank = fields.Integer('Rank', help="Add the Order of print out")
